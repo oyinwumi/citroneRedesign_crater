2336840422f936782598e8d54f531464327b5207
“Oyindamola” <oyinwumi0229@gmail.com>
2023-03-19 00:54:15 +0100

    landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/pages/landingPage/LandingPage";
 
 
 function App() {
   return (
     <div className="font-font">
-    
+     <Router>
+       <Routes>
+         <Route path='/' element={ <LandingPage/>}/>
+       </Routes>
+     </Router>
+   
     </div>
   );
 }
