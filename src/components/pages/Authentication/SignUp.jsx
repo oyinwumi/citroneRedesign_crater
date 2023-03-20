8a034ed16e903fc2fd382344e83320354a3cb587
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-20 08:16:20 +0100

    corrected the spelling of assets, added some svg files to assets folder and also created a new folder and some new files

---
+++ src/components/pages/Authentication/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "../../../assets/logo.svg";
+import Person from "../../../assets/person-icon.svg";
+import Mail from "../../../assets/mail-icon.svg";
+import Call from "../../../assets/call-icon.svg";
+import Lock from "../../../assets/lock-icon.svg";
+import Eye from "../../../assets/eye-icon.svg";
+
+const SignUp = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h1></h1>
+    </div>
+  );
+};
+
+export default SignUp;
