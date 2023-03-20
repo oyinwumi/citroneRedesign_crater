49594336f1cbded9da1ebc97e18b2fbec4707c41
“Oyindamola” <oyinwumi0229@gmail.com>
2023-03-20 19:52:43 +0100

    landing page responsiveness

--- src/components/pages/landingPage/LandingPage.jsx
+++ src/components/pages/landingPage/LandingPage.jsx
@@ -21,19 +21,19 @@ const LandingPage = () => {
                 <button className='border border-x-purple text-center md:py-3 py-2 md:px-4 px-0 md:w-[102px] w-14 text-white md:text-sm text-xs rounded-lg bg-purple ml-3  hover:bg-purple hover:opacity-50'>SIGNUP</button>
             </div>
         </nav>
-        <div className='flex lg:flex-row flex-col  md:leading-[64px] bg-light md:px-32 px-10 pt-20 '>
+        <div className='flex lg:flex-row flex-col  md:leading-[64px] bg-light md:px-32 px-10 pt-20'>
           <div className='md:w-1/2 w-full lg:mt-14 md:mt-0'>
-             <h1 className='text-black md:text-[50px] text-[40px] font-bold md:w-[500px] w-full md:mt-10 mt-0'>Deliver a <span className='text-purple'>Community-First</span> Learning Experience</h1>
-             <p className='text-black text-lg lg:w-[600px] md:w-full w-full mt-8'>Citrone makes it easy for educators to create learning experiences that positively engage students.</p>
+             <h1 className='text-black  md:text-[50px] text-[40px] font-bold  lg:w-[500px] md:w-[500px] w-full md:mt-10 mt-0'>Deliver a <span className='text-purple'>Community-First</span> Learning Experience</h1>
+             <p className='text-black text-base  lg:w-[500px] md:w-full w-full mt-8'>Citrone makes it easy for educators to create learning experiences that positively engage students.</p>
              <div className='my-10 md:flex-none flex '>
                 <button className='border border-x-purple text-center py-3  px-4 w-[184px] text-white md:text-sm text-xs rounded-lg bg-purple  hover:bg-purple hover:opacity-50'>Get Started for Free</button>
                 <button className='border border-x-purple text-center py-3  px-4 w-[184px]  ml-3 text-purple md:text-sm text-xs rounded-lg bg-white hover:bg-purple hover:text-white'>Learn more</button>
              </div>
           </div>
-          <div className='relative lg:ml-28 md:ml-40 ml-14 md:w-1/2 w-full md:mt-0 mt-14 items-center'>
-            <img src={PrettyLady} alt={PrettyLady} className='h-auto lg:w-full md:w-11/12 w-full items-center'/>
+          <div className='relative lg:ml-40 md:ml-40 ml-14 md:w-1/2 w-full md:mt-0 mt-14 items-center'>
+            <img src={PrettyLady} alt={PrettyLady} className=' xl:h-auto lg:h-full xl-w-5/12 lg:w-full md:w-11/12 w-full items-center'/>
             <img src={Jet} alt={Jet}  className='absolute top-0 md:left-[-32] left-10 md:w-[80px] w-[80px]'/>
-            <img src={Ellipse} alt={Ellipse} className='absolute lg:bottom-40 md:bottom-10 bottom-12 lg:left-0 md:w-[50px] w-[50px]' />
+            <img src={Ellipse} alt={Ellipse} className='absolute lg:bottom-10 md:bottom-10 bottom-12 lg:left-0 md:w-[50px] w-[50px]' />
           </div>
         </div>
         <section className='my-14 md:px-32 px-10'>
@@ -57,32 +57,34 @@ const LandingPage = () => {
                </div>
             </div>
         </section>
-        <section className='flex  lg:flex-row  md:col flex-col py-14 md:px-32 px-10 justify-between mb-14'>
-            <img src={MockupOne} alt={MockupOne} className=' lg:w-5/12 w-full h-auto'/>
-           <div className='lg:w-1/2 md:w-full w-full md:mt-20 mt-12'>
-           <h3 className='text-purple md:text-[40px] text-3xl font-extrabold mb-2'>Community</h3>
-            <p className='text-black font-semibold md:text-[32px] text-2xl mb-2 '>Nurture a community-first learning experience.</p>
+        <section className='flex  lg:flex-row  md:flex-col flex-col py-14 md:px-32 px-10 justify-between mb-14'>
+            <img src={MockupOne} alt={MockupOne} className=' lg:w-5/12 w-full h-auto lg:order-1 md:order-2 order-2 lg:mt-0 md:mt-14 mt-14'/>
+           <div className='lg:w-1/2 md:w-full w-full md:mt-20 mt-12 lg:order-2 md:order-1 order-1 lg:mb-0 md:mb-14' >
+           <h3 className='text-purple md:text-[40px] text-3xl font-extrabold mb-6'>Community</h3>
+            <p className='text-black font-semibold md:text-[32px] text-2xl mb-6 '>Nurture a community-first learning experience.</p>
             <p className='md:text-xl '>Students who feel connected to their learning community are more likely to persist. We foster meaningful connection by ensuring students are never more than 1 click away from their peers and educators.</p>
            </div>
         </section>
         <section className='flex lg:flex-row md:flex-col flex-col pb-14 md:px-32 px-10  justify-between'>
         <div className=' lg:w-1/2 md:w-full w-full md:mt-20 mt-12 lg:mb-0 md:mb-20' >
-           <h3 className='text-purple md:text-[40px] text-3xl font-extrabold mb-2'>Effective Learning</h3>
-            <p className='text-black font-semibold tmd:text-[32px] text-2xl mb-2 '>Engaging and effective training.</p>
+           <h3 className='text-purple md:text-[40px] text-3xl font-extrabold mb-6'>Effective Learning</h3>
+            <p className='text-black font-semibold tmd:text-[32px] text-2xl mb-6 '>Engaging and effective training.</p>
             <p className='md:text-xl'>Engage learners with video interactions, note-taking, slides, e-books, self-assessment, certificates, and much more.</p>
            </div>
            <img src={Mockuptwo} alt={Mockuptwo} className=' lg:w-5/12 md:w-full w-full md:mt-0 mt-14 h-auto'/>
         </section>
-        <section className='mt-32  mb-20 relative' >
-           <div className='flex  lg:flex-row  md:flex-col flex-col px-32'>
-            <img src={Mockupthree} alt={Mockupthree} className=' xl:w-6/12 lg:w-full md:w-full w-full md:h-auto h-full lg:order-1 order-2 ' />
-            <p className='text-purple text-[40px] md:ml-20 ml-0  lg:order-2 order-1 lg:mb-0 mb-10'>Reporting</p>
+            <section className='flex  lg:flex-row  md:col flex-col py-14 md:px-32 px-10 justify-between mb-14'>
+            <img src={Mockupthree} alt={Mockupthree} className=' lg:w-5/12 w-full h-auto  lg:order-1 md:order-2 order-2 lg:mt-0 md:mt-14 mt-14'/>
+           <div className='lg:w-1/2 md:w-full w-full md:mt-20 mt-12 lg:order-2 md:order-1 order-1 lg:mb-0 md:mb-14'>
+           <h3 className='text-purple md:text-[40px] text-3xl font-extrabold mb-6'>Reporting</h3>
+            <p className='text-black font-semibold md:text-[32px] text-2xl mb-6 '>Nurture a Engagement Insights.</p>
+            <p className='md:text-xl '>Quickly see which students are engaged and which are not, so you can take timely action to support students directly from Citrone.</p>
            </div>
-           <div className='bg-light  absolute xl:bottom-0 lg:bottom-0 md:-bottom-10 -bottom-10 w-full md:h-80 h-40 '>
-            <h3 className='text-center text-black md:text-[40px] text-xl md:pt-12 pt-6 mb-4 '>Want to start learning <br /> easily today?</h3>
+        </section>
+         <div className='bg-light items-center mb-20'>
+            <h3 className='text-center text-black md:text-[40px] text-xl md:pt-12 pt-6 mb-4  leading-10'>Want to start learning <br /> easily today?</h3>
              <button className='items-center mb-16 md:mx-[45%] mx-[35%] md:mt-6 mt-0 border border-x-purple text-center py-3 px-4 w-[184px] text-white text-sm rounded-lg bg-purple  hover:bg-purple hover:opacity-50'>Get Started for Free</button> 
            </div> 
-        </section>
         <footer className='bg-white md:px-32 px-10 pb-10 flex  lg:flex-row flex-col justify-between mt-10'>
              <div className='flex md:flex-row flex-col lg:w-3/5 w-full lg:order-1 order-2 lg:mt-0 mt-10'>
                 <div className=' text-justify text-black md:w-1/4 w-full md:mr-4 mr-0'>
