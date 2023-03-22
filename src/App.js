484518c96fa0cabb52d4f887e082373ca671b7b4
“Oyindamola” <oyinwumi0229@gmail.com>
2023-03-22 08:58:18 +0100

    landing page responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/pages/landingPage/LandingPage';
 import SignUp from './components/pages/Authentication/SignUp';
 import LogIn from './components/pages/Authentication/LogIn';
+import CoursePage from './components/pages/coursePage/Course';
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<LandingPage />}></Route>
+          <Route path='/course' element={ <CoursePage/>}/>
           <Route exact path='/signup' element={<SignUp />}></Route>
           <Route exact path='/login' element={<LogIn />}></Route>
         </Routes>
       </Router>
-    </div>
+      </div>
+
   );
 }
 
---
+++ src/components/pages/coursePage/Course.jsx
@@ -0,0 +1,36 @@
+
+import Logo from '../../../assets/logo.svg';
+import ProfileImage from '../../../assets/profileImage.svg'
+import { FaSearch , FaRegBell, FaCog} from "react-icons/fa";
+
+const Course = () => {
+  return (
+    <div className='lg:px-32 md:px-10 '>
+        <nav className='flex h-[88px] items-center  border justify-between'>
+           <img src={Logo} alt={Logo}  className='lg:w-auto md:w-[80px] w-[50px]'/>
+           <div className='relative'>
+              <span className='text-lightgrey text-xl absolute top-3 left-2 '><FaSearch/></span><input type="text" placeholder='search'  className='border rounded-lg  border-lightgrey lg:w-[300px] md:w-[200px]  w-[150px] placeholder:pl-8 h-10'/>
+           </div>
+           <div className='flex items-center'>
+            <div className='class relative'>
+             <span className='text-lightgrey text-2xl'>< FaRegBell/> </span>
+            <div className='w-4 h-4 rounded-full border  text-center absolute bottom-4 left-5 bg-lightpurple'> <p className='text-xs text-purple'>0</p></div>
+            </div>
+            <span className='text-2xl text-black ml-4'><FaCog/></span>
+            <div className='flex items-center ml-4'>
+                <div className=''>
+                    <img src={ProfileImage} alt={ProfileImage} />
+                    <span className='w-2 h-2 rounded-full bg-lemongreen'></span>
+                </div>
+                <div className='flex flex-col ml-2 '>
+                <header className='text-xs'>User Fullname</header>
+                <p className='text-xs'>online</p>
+                </div>
+            </div>
+           </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Course
