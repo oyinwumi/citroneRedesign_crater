59bb7da5bf790b2a438bb614aea3755f804b3808
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-22 11:16:13 +0100

    I was actually working on the ForgotPassword page and NOT the ResetPassword page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import LandingPage from './components/pages/landingPage/LandingPage';
 import SignUp from './components/pages/Authentication/SignUp';
 import LogIn from './components/pages/Authentication/LogIn';
+import ForgotPassword from './components/pages/Authentication/ForgotPassword';
 import ResetPassword from './components/pages/Authentication/ResetPassword';
 
 function App() {
@@ -13,6 +14,11 @@ function App() {
           {/* <Route exact path='/' element={<LandingPage />}></Route> */}
           <Route exact path='/' element={<SignUp />}></Route>
           <Route exact path='/login' element={<LogIn />}></Route>
+          <Route
+            exact
+            path='/forgot-password'
+            element={<ForgotPassword />}
+          ></Route>
           <Route
             exact
             path='/reset-password'
---
+++ src/components/pages/Authentication/ForgotPassword.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/logo.svg';
+import Mail from '../../../assets/mail-icon.svg';
+
+const ForgotPassword = () => {
+  return (
+    <div className='bg-light h-screen flex justify-center items-center'>
+      <div className='w-148 h-200 bg-white '>
+        <img src={Logo} alt='' className='ml-6 mt-8 cursor-pointer' />
+
+        <div className='px-16 mt-12'>
+          <h1 className='text-center text-black text-xl font-bold'>
+            Forgot Password
+          </h1>
+          <p className='text-center mt-4 text-black'>
+            Enter the email registered with your account
+          </p>
+
+          <form action=''>
+            <div className='flex items-center bg-white mt-8 border border-lightgrey rounded'>
+              <img
+                src={Mail}
+                alt=''
+                className='bg-light px-3 py-3.5 rounded-l'
+              />
+              <input
+                type='text'
+                name='email'
+                placeholder='Enter your email'
+                className='w-full rounded-r ml-3 placeholder:text-black focus: outline-0'
+              />
+            </div>
+
+            <button
+              type='submit'
+              className='bg-purple flex justify-center items-center w-full mt-8 py-3 font-bold text-white rounded'
+            >
+              Continue
+            </button>
+          </form>
+
+          <div className='flex justify-center items-center mt-5'>
+            <p className='mr-4'>Don't have an account?</p>
+            <Link to='/signup' className='text-purple font-semibold'>
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
