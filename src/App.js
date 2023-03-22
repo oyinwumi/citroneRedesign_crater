eb5721da4a8351048b8e20e3074e5fdec8b919bd
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-22 11:39:49 +0100

    completed the design of the ResetPassword page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import LandingPage from './components/pages/landingPage/LandingPage';
-import SignUp from './components/pages/Authentication/SignUp';
-import LogIn from './components/pages/Authentication/LogIn';
-import ForgotPassword from './components/pages/Authentication/ForgotPassword';
+// import SignUp from './components/pages/Authentication/SignUp';
+// import LogIn from './components/pages/Authentication/LogIn';
+// import ForgotPassword from './components/pages/Authentication/ForgotPassword';
 import ResetPassword from './components/pages/Authentication/ResetPassword';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          {/* <Route exact path='/' element={<LandingPage />}></Route> */}
-          <Route exact path='/' element={<SignUp />}></Route>
+      <ResetPassword />
+      {/* <Router>
+        <Routes> */}
+      {/* <Route exact path='/' element={<LandingPage />}></Route> */}
+      {/* <Route exact path='/' element={<SignUp />}></Route>
           <Route exact path='/login' element={<LogIn />}></Route>
           <Route
             exact
@@ -23,9 +24,9 @@ function App() {
             exact
             path='/reset-password'
             element={<ResetPassword />}
-          ></Route>
-        </Routes>
-      </Router>
+          ></Route> */}
+      {/* </Routes>
+      </Router> */}
     </div>
   );
 }
