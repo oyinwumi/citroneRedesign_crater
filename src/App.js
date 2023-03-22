833234f55250c853c51b6c6d2d8d23c9e850c135
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-22 18:10:19 +0100

    completed the SuccessPopUp and ErrorPopUp. I also added some code to the app.js and tailwind configuration file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import LandingPage from './components/pages/landingPage/LandingPage';
-// import SignUp from './components/pages/Authentication/SignUp';
-// import LogIn from './components/pages/Authentication/LogIn';
-// import ForgotPassword from './components/pages/Authentication/ForgotPassword';
-import ResetPassword from './components/pages/Authentication/ResetPassword';
+import LandingPage from './components/pages/landingPage/LandingPage';
+import SignUp from './components/pages/Authentication/SignUp';
+import LogIn from './components/pages/Authentication/LogIn';
+import ForgotPassword from './components/pages/Authentication/ForgotPassword';
 
 function App() {
   return (
     <div>
-      <ResetPassword />
-      {/* <Router>
-        <Routes> */}
-      {/* <Route exact path='/' element={<LandingPage />}></Route> */}
-      {/* <Route exact path='/' element={<SignUp />}></Route>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<LandingPage />}></Route>
+          <Route exact path='/signup' element={<SignUp />}></Route>
           <Route exact path='/login' element={<LogIn />}></Route>
           <Route
             exact
             path='/forgot-password'
             element={<ForgotPassword />}
           ></Route>
-          <Route
-            exact
-            path='/reset-password'
-            element={<ResetPassword />}
-          ></Route> */}
-      {/* </Routes>
-      </Router> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Authentication/SuccessPopUp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaCheck } from 'react-icons/fa';
+
+const SuccessPopUp = () => {
+  return (
+    <div className='flex h-screen justify-center items-center bg-grey opacity-50'>
+      <div className='w-120 h-61 bg-white flex flex-col items-center rounded-2xl'>
+        <div className=' mt-7 border-[1.5px] border-green text-green font-light p-3 rounded-full'>
+          <FaCheck />
+        </div>
+
+        <h3 className='text-2xl text-[#000] font-semibold mt-7'>Success!</h3>
+        <p className='mt-2 text-base text-[#000]'>
+          Your password has been changed
+        </p>
+        <button className='flex justify-center items-center bg-purple w-[86px] h-[42px] text-white text-base font-bold mt-6 rounded'>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPopUp;
