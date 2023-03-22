490d1fe17eaeebb07067918ff90d2ffc9a85ace5
“Oyindamola” <oyinwumi0229@gmail.com>
2023-03-22 08:58:18 +0100

    landing page responsiveness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/pages/landingPage/LandingPage";
+import CoursePage from './components/pages/coursePage/Course'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
      <Router>
        <Routes>
          <Route path='/' element={ <LandingPage/>}/>
+         <Route path='/course' element={ <CoursePage/>}/>
        </Routes>
      </Router>
    
