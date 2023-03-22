49ccb3369a03db930a5eef43c78f6cec85b4b6a9
ichiomafrancis <ichiomafrancis@gmail.com>
2023-03-22 11:08:13 +0100

    completed the design of ResetPassword page

--- src/components/pages/Authentication/ResetPassword.jsx
+++ src/components/pages/Authentication/ResetPassword.jsx
@@ -5,11 +5,9 @@ import Mail from '../../../assets/mail-icon.svg';
 
 const ResetPassword = () => {
   return (
-    <div className='bg-lightpink flex justify-center items-center'>
-      <div className='w-148 h-200 bg-green '>
-        <div className='ml-6 mt-8'>
-          <img src={Logo} alt='' />
-        </div>
+    <div className='bg-light h-screen flex justify-center items-center'>
+      <div className='w-148 h-200 bg-white '>
+        <img src={Logo} alt='' className='ml-6 mt-8 cursor-pointer' />
 
         <div className='px-16 mt-12'>
           <h1 className='text-center text-black text-xl font-bold'>
@@ -20,8 +18,12 @@ const ResetPassword = () => {
           </p>
 
           <form action=''>
-            <div className='flex bg-white mt-8 border border-lightgrey rounded'>
-              <img src={Mail} alt='' className='bg-light p-3 rounded-l' />
+            <div className='flex items-center bg-white mt-8 border border-lightgrey rounded'>
+              <img
+                src={Mail}
+                alt=''
+                className='bg-light px-3 py-3.5 rounded-l'
+              />
               <input
                 type='text'
                 name='email'
@@ -32,15 +34,17 @@ const ResetPassword = () => {
 
             <button
               type='submit'
-              className='bg-purple w-full mt-8 py-3.5 text-white rounded'
+              className='bg-purple flex justify-center items-center w-full mt-8 py-3 font-bold text-white rounded'
             >
               Continue
             </button>
           </form>
 
-          <div className='flex'>
-            <p>Don't have an account?</p>
-            <Link to='/signup'>Sign Up</Link>
+          <div className='flex justify-center items-center mt-5'>
+            <p className='mr-4'>Don't have an account?</p>
+            <Link to='/signup' className='text-purple font-semibold'>
+              Sign Up
+            </Link>
           </div>
         </div>
       </div>
