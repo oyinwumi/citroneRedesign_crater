ed23767e2385e50ce6829c131e11ceac4d4eb261
Francis Chioma <ichiomafrancis@gmail.com>
2023-03-29 22:43:56 +0100

    commented out some codes

--- src/apps/store.jsx
+++ src/apps/store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userReducer';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     userReducer,
   },
 });
-
-export default store;
