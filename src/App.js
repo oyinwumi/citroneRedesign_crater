aec675d2facff87faed4120ed29b7e6972355abf
Francis Chioma <ichiomafrancis@gmail.com>
2023-04-05 07:58:42 +0100

    renamed Authentication to authentication"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/pages/landingPage/LandingPage';
-import SignUp from './components/pages/Authentication/SignUp';
-import LogIn from './components/pages/Authentication/LogIn';
+import SignUp from './components/pages/authentication/SignUp';
+import LogIn from './components/pages/authentication/LogIn';
 import CoursePage from './components/pages/coursePage/Course';
-import ForgotPassword from './components/pages/Authentication/ForgotPassword';
+import ForgotPassword from './components/pages/authentication/ForgotPassword';
 import Beginner from './components/pages/coursePage/beginner/Beginner';
 import ModuleOne from './components/pages/courseModule/ModuleOne';
 import Quiz from './components/pages/courseModule/quiz/Quiz';
@@ -22,15 +22,15 @@ function App() {
         <Route path='/course' element={<CoursePage />} />
         <Route path='/beginner' element={<Beginner />} />
         <Route path='/moduleone' element={<ModuleOne />} />
-        <Route path='/quiz' element={<Quiz/>} />
-        <Route path='/assignment' element={<Assignment/>} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/assignment' element={<Assignment />} />
         <Route exact path='/signup' element={<SignUp />} />
         <Route exact path='/login' element={<LogIn />} />
         <Route exact path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/sidebar' element={<Sidebar/>}/>
-        <Route path='/assignmentqest' element={<AssignmentQest/>}/>
-        <Route path='/scoreboard' element={<Scoreboard/>} />
-        <Route path='/quizmodalone' element= {<QuizModalOne/>} />
+        <Route path='/sidebar' element={<Sidebar />} />
+        <Route path='/assignmentqest' element={<AssignmentQest />} />
+        <Route path='/scoreboard' element={<Scoreboard />} />
+        <Route path='/quizmodalone' element={<QuizModalOne />} />
       </Routes>
     </div>
   );
