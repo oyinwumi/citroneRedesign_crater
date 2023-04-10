fcc3fd82af1ef2401c1e851efde1f8d4d3f23e7e
“Oyindamola” <oyinwumi0229@gmail.com>
2023-04-10 15:16:39 +0100

    dashboard continuation two

---
+++ src/components/dashboard/activity.js
@@ -0,0 +1,12 @@
+import ActivityIcon from '../../assets/activity.svg';
+import Timer from '../../assets/timer.svg';
+import ProfileIcon from '../../assets/profileIcon.svg';
+
+const recentActivity = [
+    { iconOne: ActivityIcon, iconTwo: Timer, iconThree: ProfileIcon , header:'Product design and design thinking process', day: 'Sept 10, 2022 -  4:00pm', level: '100%', profileImage: '', profileName: 'Timmy Adubi'},
+    { iconOne: ActivityIcon, iconTwo: Timer, iconThree: ProfileIcon , header:'Product design and design thinking process', day: 'Sept 10, 2022 -  4:00pm', level: '100%', profileImage: '', profileName: 'Timmy Adubi'},
+    { iconOne: ActivityIcon, iconTwo: Timer, iconThree: ProfileIcon , header:'Product design and design thinking process', day: 'Sept 10, 2022 -  4:00pm', level: '100%', profileImage: '', profileName: 'Timmy Adubi'},
+    { iconOne: ActivityIcon, iconTwo: Timer, iconThree: ProfileIcon , header:'Product design and design thinking process', day: 'Sept 10, 2022 -  4:00pm', level: '100%', profileImage: '', profileName: 'Timmy Adubi'},
+];
+
+export default recentActivity
